9940e87b55cbe0939df7528ad201b39edff652e0
jesther-ai <jesthersilvestre@yahoo.com>
2022-02-26 08:42:02 +0800

    [update]

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Utilities/hex_color.dart';
 import 'package:news_app/widgets/sliver_app_bar.dart';
+
+import '../widgets/home_footer.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return NewsSliverAppBar(
-      body: Text('body'),
-      imageUrl: 'https://github.com/jesther-ai/magazine_v1.1/raw/master/screenshot/sampleV2.png?raw=true',
-      footer: Text('Footer'),
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: NewsSliverAppBar(
+        body: const HomeBody(),
+        imageUrl: 'https://github.com/jesther-ai/magazine_v1.1/raw/master/screenshot/sampleV2.png?raw=true',
+        footer: const HomeFooter(),
+      ),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: HexColor('#DBF9FF'),
+      child: const TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          SizedBox(),
+          SizedBox(),
+          SizedBox(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/home_footer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/Utilities/hex_color.dart';
+
+class HomeFooter extends StatelessWidget {
+  const HomeFooter({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.circular(20),
+        ),
+      ),
+      padding: const EdgeInsets.only(bottom: 25),
+      height: 110,
+      child: Center(
+        child: TabBar(
+          indicatorSize: TabBarIndicatorSize.label,
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          labelPadding: const EdgeInsets.only(left: 20, right: 20),
+          labelColor: HexColor('#8855d7'),
+          unselectedLabelStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.normal,
+            fontSize: 15,
+            color: HexColor('#7D9297'),
+          ),
+          unselectedLabelColor: HexColor('#7D9297'),
+          indicatorColor: HexColor('#8855d7'),
+          labelStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.normal,
+            fontSize: 15,
+            color: HexColor('#006B83'),
+          ),
+          isScrollable: true,
+          tabs: const [
+            Tab(child: Text('Account'), icon: Icon(Icons.person_outline)),
+            Tab(child: Text('Home'), icon: Icon(Icons.home)),
+            Tab(child: Text('Favorites'), icon: Icon(Icons.favorite_border_rounded)),
+          ],
+        ),
+      ),
+    );
+  }
+}
